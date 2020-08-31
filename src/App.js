594187fc2d15bf4fa77766eb0ecb23a74466db8e
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-08-31 17:37:33 +0100

    Added prettier to indent code. App component indentation changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                My App Component
-   </div>
-        );
-    }
+  render() {
+    return <div>My App Component</div>;
+  }
 }
-export default App
+export default App;
