aa5c1e8f91119bc826880323a563321f21b847e7
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-08-31 18:38:17 +0100

    Added typescript to project. Changed folder structure and some webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const path = require( 'path' );
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-   context: __dirname,
-   entry: './src/index.js',
-   output: {
-      path: path.resolve( __dirname, 'dist' ),
-      filename: 'main.js',
-      publicPath: '/',
-   },
-   devServer: {
-      historyApiFallback: true
-   },
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            use: 'babel-loader',
-         },
-         {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.(png|j?g|svg|gif)?$/,
-            use: 'file-loader'
-         }
-]
-   },
-   plugins: [
-      new HtmlWebPackPlugin({
-         template: path.resolve( __dirname, 'public/index.html' ),
-         filename: 'index.html'
-      })
-   ]
+  context: __dirname,
+  entry: "./src/components/App/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/yourfile.css"
+    })
+  ]
 };
