3c643490a4a02f26712849e96e74cd8df7cb9d07
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-09-02 20:34:18 +0100

    Deleted launchMapper file cause its function was copied to inside the LaunchClass. This way, ILaunch interface is not needed, so it was also deleted

--- src/models/launchClass.ts
+++ src/models/launchClass.ts
@@ -1,3 +1,4 @@
+import { Classification } from "../Constants";
 export default class LaunchClass {
   id: string;
   img?: string;
@@ -11,4 +12,46 @@ export default class LaunchClass {
   rocketName?: string; //field obtain through the rocket API with the id
 
   constructor() {}
+
+  public jsonToLaunchMapper(launchJSON: any) {
+    if (launchJSON.id) {
+      this.id = launchJSON.id;
+    }
+    if (
+      launchJSON.links &&
+      launchJSON.links.flickr &&
+      launchJSON.links.flickr.original &&
+      launchJSON.links.flickr.original.length > 0
+    ) {
+      this.img = launchJSON.links.flickr.original[0];
+    }
+    if (launchJSON.name) {
+      this.name = launchJSON.name;
+    }
+    if (launchJSON.date_utc) {
+      this.date = launchJSON.date_utc;
+    }
+    if (launchJSON.details) {
+      this.details = launchJSON.details;
+    }
+    if (launchJSON.success != null) {
+      this.success = launchJSON.success;
+    }
+    if (launchJSON.upcoming != null) {
+      this.upcoming = launchJSON.upcoming;
+    }
+    if (launchJSON.rocket) {
+      this.rocketId = launchJSON.rocket;
+    }
+
+    /* fill missing object parameters by logic*/
+    //classification:
+    if (this.success) {
+      this.classification = Classification.SUCCESS;
+    } else if (this.upcoming) {
+      this.classification = Classification.UPCOMING;
+    } else {
+      this.classification = Classification.FAILED;
+    }
+  }
 }
