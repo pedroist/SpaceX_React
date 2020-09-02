6d207ccaca9f559bab0ddb6ba9cea6b59a90ebf7
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-09-02 23:10:09 +0100

    Cardlist connected to redux and using launches array to map it and render dynamically each Card component

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
+import { connect } from "react-redux";
 import Card from "../card/card";
+import ILaunch from "../../models/ILaunch";
 
-type Props = {};
+type Props = {
+  hasArray: Boolean;
+  launches: Array<ILaunch>;
+};
 
 const CardList: React.FC<Props> = props => {
+  const { launches } = props;
   return (
     <div>
-      <h1>Card List</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {(!launches || launches.length == 0) && <div>Loading...</div>}
+      {launches &&
+        launches.length > 0 &&
+        launches.map(launch => <Card key={launch.id} />)}
     </div>
   );
 };
 
-export default CardList;
+const mapStateToProps = state => {
+  return {
+    hasLaunches: state.reduxSaga.hasLaunches,
+    launches: state.reduxSaga.launches
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(CardList);
