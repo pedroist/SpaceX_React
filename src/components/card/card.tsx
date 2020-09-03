4eeea622c7fd16f3c7b766206882000048269caf
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2020-09-03 02:53:16 +0100

    Changed Func Component Card to a Class Component connect in order to use ComponentDidMount to dispatch action that will make Saga load the Rocket correspondent. The purpose is to fill the missing info on the card.

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,33 +1,67 @@
 import * as React from "react";
+import { connect } from "react-redux";
 import "./card.scss";
 import ILaunch from "../../models/ILaunch";
+import launches from "../../pages/launches";
 
-type Props = { launch: ILaunch };
+type OwnProps = { launch: ILaunch };
 
-const Card: React.FC<Props> = props => {
-  const { launch } = props;
-  return (
-    <div className="card">
-      <div
-        className="card-img"
-        style={{
-          background: `url(${launch.img}) 0% 50% / contain`
-        }}
-      ></div>
-      {/* <img className="card-img" src={launch.img} alt="card" /> */}
-      <div className="card-content">
-        <div className="card-body">
-          <div className="card-title">{launch.name}</div>
-          <div className="card-subtitle">{launch.date}</div>
-          <div className="card-description">{launch.details}</div>
-        </div>
-        <div className="card-footer">
-          <div className="card-footer-info">{launch.classification}</div>
-          <div className="card-footer-info">{launch.rocketName}</div>
-        </div>
-      </div>
-    </div>
-  );
+type Props = {
+  fetching: Boolean;
+  data: Array<any>;
+  error: string;
+  onFetchData(): any;
 };
 
-export default Card;
+type State = {};
+
+class Card extends React.Component<Props & OwnProps, State> {
+  componentDidMount() {
+    this.props.onFetchData();
+  }
+
+  render() {
+    const { launch } = this.props;
+    return (
+      <div className="card">
+        <div
+          className="card-img"
+          style={{
+            background: `url(${launch.img}) 0% 50% / contain`
+          }}
+        ></div>
+        <div className="card-content">
+          <div className="card-body">
+            <div className="card-title">{launch.name}</div>
+            <div className="card-subtitle">{launch.date}</div>
+            <div className="card-description">{launch.details}</div>
+          </div>
+          <div className="card-footer">
+            <div className="card-footer-info">{launch.classification}</div>
+            <div className="card-footer-info">{launch.rocketName}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    fetching: state.reduxSaga.fetching,
+    data: state.reduxSaga.data,
+    error: state.reduxSaga.error,
+    launch: ownProps.launch
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchData: () => dispatch({ type: "API_ROCKETS_REQUEST" })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Card);
